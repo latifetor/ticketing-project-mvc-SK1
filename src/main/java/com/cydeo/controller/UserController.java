db0f38fa89279462688739a98481c17ad1a2080b
supawadeek <latifetor@gmail.com>
2023-10-16 17:38:31 -0400

    th:field created

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.UserDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ public class UserController {
     @GetMapping("/create")
     public String createUser(Model model){
 
+        model.addAttribute("user",new UserDTO());
 
         return "/user/create";
     }
