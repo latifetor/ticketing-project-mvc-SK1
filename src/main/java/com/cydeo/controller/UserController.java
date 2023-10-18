2c361cdcfd22c9fe328471c814718aecd55029da
supawadeek <latifetor@gmail.com>
2023-10-17 23:37:11 -0400

    delete function built

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -60,16 +60,16 @@ public class UserController {
         return "redirect:/user/create";
 
     }
-//
-//
-//    @GetMapping("/delete/{username}")
-//    public String deleteUser(@PathVariable("username") String username){
-//
-//        userService.deleteById(username);
-//
-//        return "redirect:/user/create";
-//    }
-//
+
+
+    @GetMapping("/delete/{username}")
+    public String deleteUser(@PathVariable("username") String username){
+
+        userService.deleteById(username);
+
+        return "redirect:/user/create";
+    }
+
 
 
 }
